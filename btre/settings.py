d74447373a20623a0fa0c9561f7b4ca124794260
Richmond Boahene <rkboahene2@gmail.com>
2021-08-13 02:30:21 +0000

    prep for heroku

--- btre/settings.py
+++ btre/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fnm)vk%9cbklt2xdgh@6=&2x6=3wp&aqk2@#+(mzz66%r*ib6e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','janelhousing.com']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
